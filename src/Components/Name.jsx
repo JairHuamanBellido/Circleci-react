2adab5639f16419111d855edd490d3330b69484c
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-03-14 15:53:47 -0500

    Configuraction webpack

---
+++ src/Components/Name.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Name extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hola, soy jair</h2>      
+            </div>
+        );
+    }
+}
+
+export default Name;
