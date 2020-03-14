e47cf454fea2ceebe8aa511889ac2cf942643eb4
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-03-14 11:03:32 -0500

    Failed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Name from "./Name";
+
+
+describe('My Test Suite', () => {
+  it('My Test Case', () => {
+    expect(false).toEqual(true);
+  });
 });
+
+
+describe('Name', ()=>{
+  it('Contain Name', ()=>{
+    const component = renderer.create(<Name name="hola"/>);
+    let tree =  component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
---
+++ src/Name.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class Name extends React.Component {
+
+    render() {
+
+
+        return (
+            <h1>Hola {this.props.name}</h1>
+        )
+
+    }
+}
