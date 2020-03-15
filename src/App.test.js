b0bb54ae8e0112546ac2d9102c96b147bcbad525
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-03-15 16:21:27 -0500

    Add dependencies for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import App from './App';
+import { expect } from "chai";
 import Enzyme from "enzyme";
-import {  shallow} from "enzyme";
+import { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
+import { Link } from 'react-router-dom';
 
 
 Enzyme.configure({ adapter: new Adapter() });
 
 
 it('renders without crashing', () => {
-  const component =  shallow(<App />);
+  const component = shallow(<App />);
 
-  expect(component.getElements()).toMatchSnapshot();
+  expect(component.contains(<Link to="/">Home</Link>)).to.equal(true);
+
 
 });
